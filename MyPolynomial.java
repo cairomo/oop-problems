9376dd7866d71e3826bb3cec2f12431d363589fb
Chloe Mo <chloe.mo@gmail.com>
2014-03-19 13:41:55 -0700

    Update MyPolynomial.java
    done except toString() sometimes has "+" at the end

--- MyPolynomial.java
+++ MyPolynomial.java
@@ -1,11 +1,5 @@
 package oneseven;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
 /*
  * EXERCISE 1.7: The MyPolynomial Class
 
@@ -29,118 +23,16 @@ How about C/C++? Why?
 In Java, the degree of the polynomial does not need to be kept as an instance variable because you can just call getDegree() whenever
 you need it.
  */
-public class MyPolynomial {
-	private double[] coeffs;
-	public MyPolynomial(double... coeffs) {
-		this.coeffs = coeffs;
-	}
-	
-	public MyPolynomial(String filename) {
-		//gets coeffs from file
-		Scanner in = null;
-		   try {
-		      in = new Scanner(new File(filename));  // open file
-		   } catch (FileNotFoundException e) {
-		      e.printStackTrace();
-		   }
-		   int degree = in.nextInt();      // read the degree
-		   coeffs = new double[degree+1];  // allocate the array
-		   for (int i=0; i<coeffs.length; ++i) {
-		      coeffs[i] = in.nextDouble();
-		   }
-	}
-	
-	public int getDegree() {
-		int endIndex = this.coeffs.length;
-		for(int i = this.coeffs.length - 1;i>0;--i) {
-			if(this.coeffs[i] == 0) {
-				continue;
-			} else {
-				endIndex = i;
-				return endIndex + 1;
-			}
-		}
-		return endIndex;
-	}
-	
-	public String toString() {
-		String ret = "";
-		for(int i = this.getDegree() - 1;i >= 0;--i) {
-			if(this.coeffs[i] != 0) {
-				if(this.coeffs[i] == 1) {
-					if(i == 1) {
-						ret += ("x" + " + " );
-					} else if(i == 0) {
-						ret += "1";
-					} else {
-						ret += ("x^" + Integer.toString(i) + " + " );
-					}
-				} else {
-					if(i == 1) {
-						ret += (Double.toString(this.coeffs[i]) + "x" + " + " );
-					} else if(i == 0) {
-						ret += (Double.toString(this.coeffs[i]) + " + " );
-					} else {
-						ret += (Double.toString(this.coeffs[i]) + "x^" + Integer.toString(i) + " + " );
-					}
-				}
-			}
-		}
-		return ret;
-	}
-	
-	public double evaluate(double x) {
-		double val = 0;
-		for(int i = 0;i < this.getDegree();++i) {
-			val += (this.coeffs[i] * (Math.pow(x, i)));
-		}
-		return val;
-	}
-	
-	public MyPolynomial add(MyPolynomial other) {
-		int retDegree = Math.max(this.getDegree(), other.getDegree());
-		
-		double[] retCoeffArray = new double[retDegree + 1];
-		
-		for(int i = 0;i<Math.min(coeffs.length,  other.coeffs.length); ++i) {
-			retCoeffArray[i] = this.coeffs[i] + other.coeffs[i];
-		}
-		
-		for(int i = Math.min(coeffs.length, other.coeffs.length); i <retDegree + 1;++i) {
-			if(coeffs.length > other.coeffs.length) {
-				retCoeffArray[i] = this.coeffs[i];
-			} else {
-				retCoeffArray[i] = other.coeffs[i];
-			}
-		}
-		MyPolynomial retVal = new MyPolynomial(retCoeffArray);
-		return retVal;
-		
-		
-		/* double[] c = new double[Math.max((this.coeffs).length, (a.coeffs).length)];
-		
-		ArrayList<Double> one = new ArrayList(Arrays.asList(c));
-		
-		for(int i = Math.max((this.coeffs).length, (a.coeffs).length);i >= Math.abs((this.coeffs).length - (a.coeffs).length);++i) {
-			//starts at end of array
-			if((this.coeffs).length >= i && (a.coeffs).length >= i) {
-				c[i] = this.coeffs[i] + a.coeffs[i];
-			} else if((this.coeffs).length >= i) {
-				c[i] = this.coeffs[i];
-			} else c[i] = a.coeffs[i];
-		}
-		MyPolynomial x = new MyPolynomial(c);
-		return x; */
-	}
-	
-	public MyPolynomial multiply(MyPolynomial a) {
-		double[] xcoeffs = new double[this.getDegree() + a.getDegree()];
-		MyPolynomial x = new MyPolynomial(xcoeffs);
-		for(int i = 0;i<this.getDegree();++i) {
-			for(int j = 0;j<a.getDegree();++j) {
-				x.coeffs[i+j] += this.coeffs[i] + a.coeffs[j];
-			}
-		}
-		return x;
+
+public class TestMyPolynomial {
+	public static void main(String[] args) {
+		double[] acoeffs = new double[] {1, 1, 0};
+		MyPolynomial a = new MyPolynomial(acoeffs);
+		System.out.println("the degree of  " + a.toString() + " is " + a.getDegree());
+		System.out.println("when x = 3, " + a.toString() + " = " + String.valueOf(a.evaluate(3.0)));
+		double[] bcoeffs = new double[] {2,1,2,4};
+		MyPolynomial b = new MyPolynomial(bcoeffs);
+		System.out.println(a.toString() + " plus " + b.toString() + " equals " + a.add(b));
+		System.out.println(a.toString() + " times " + b.toString() + " equals " + a.multiply(b));
 	}
 }
