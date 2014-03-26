ecd2623a7879a16267e890afae7405b632234465
Chloe Mo <chloe.mo@gmail.com>
2014-03-26 11:31:53 -0700

    Update Book.java
    started exercise 12

--- Book.java
+++ Book.java
@@ -9,14 +9,24 @@ A new method printAuthors() to print the names of all the authors.
 You are required to:
 Write the code for the Book class. You shall re-use the Author class written earlier.
 Write a test program (called TestBook) to test the Book class.
+
+EXERCISE 1.12
+In the above exercise, the number of authors cannot be changed once a Book instance is constructed. Suppose that we wish to allow the user to add more authors (which is really unusual but presented here for academic purpose).
+We shall remove the authors from the constructors, and add a new method called addAuthor() to add the given Author instance to this Book.
+We also need to pre-allocate an Author array, with a fixed length (says 5 - a book is written by 1 to 5 authors), and use another instance variable numAuthors (int) to keep track of the actual number of authors.
+You are required to:
+Modify your Book class to support this new requirement.
+Try writing a method called removeAuthorByName(authorName), that remove the author from this Book instance if authorName is present. The method shall return true if it succeeds.
+
  */
 
 package exercise10;
 import ex1.Author;
 
 public class Book {
-	String name;
-	Author[] author;
+	private String name;
+	private Author[] author = new Author[5];
+	private int numAuthors = 0;
 	double price;
 	int qtyInStock;
 	
@@ -31,6 +41,7 @@ public class Book {
 		this.price = price;
 		this.qtyInStock = qtyInStock;
 	}
+	
 	
 	public String getName(){
 		return this.name;
@@ -62,6 +73,11 @@ public class Book {
 		String ret = "'" + this.name + "' by " + author.length + " author(s)";
 		return ret;
 	}
+	
+	public void addAuthor(Author author) {
+	      this.getAuthors()[numAuthors] = author;
+	      ++numAuthors;
+	   }
 	
 	public void printAuthors() {
 		for(int i = 0;i<this.getAuthors().length - 1;++i) {
